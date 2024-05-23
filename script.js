9479ff4c4b1c6b005a82e33c202a4212fbab49a1
wachidmuslih <wachidmslh@gmail.com>
2024-05-23 22:30:33 +0700

    Submission Tugas Akhir Dicoding Belajar Dasar Pemrograman Web

---
+++ script.js
@@ -0,0 +1,27 @@
+const copyrightYear = document.getElementById('copyright-year');
+copyrightYear.textContent = new Date().getFullYear();
+
+var acc = document.getElementsByClassName("accordion-label");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
